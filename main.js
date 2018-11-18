571938d8059865d7074829162130fd95294152fe
Pedro Cacique <phcacique@gmail.com>
2018-11-18 11:48:13 -0200

    Icon on sidebar
    Added an icon on sidebar to access dialog menu

--- main.js
+++ main.js
@@ -7,60 +7,76 @@ define(function (require, exports, module) {
         EditorManager = brackets.getModule("editor/EditorManager"),
         Dialogs = brackets.getModule("widgets/Dialogs"),
         DefaultDialogs = brackets.getModule("widgets/DefaultDialogs"),
-        AppInit = brackets.getModule("utils/AppInit");
-    
+        AppInit = brackets.getModule("utils/AppInit"),
+        ExtensionUtils = brackets.getModule('utils/ExtensionUtils'),
+        FileSystem = brackets.getModule('filesystem/FileSystem'),
+        FileUtils = brackets.getModule('file/FileUtils');
+
     //REQUIRED
     var modal = require('text!html/modal.html');
-    var elements = [{name:"basic_html", element: require('text!html/basic_page.html')},
-                    {name:"basic_canvas", element: require('text!html/basic_canvas.html')},
-                    {name:"link_css", element: require('text!html/link_css.html')},
+    var elements = [{
+            name: "basic_html",
+            element: require('text!html/basic_page.html')
+        },
+        {
+            name: "basic_canvas",
+            element: require('text!html/basic_canvas.html')
+        },
+        {
+            name: "link_css",
+            element: require('text!html/link_css.html')
+        },
                    ];
-    
+
     var currentDoc = DocumentManager.getCurrentDocument();
     var editor = EditorManager.getCurrentFullEditor();
     var myDialog;
-    
-    
+
+    ExtensionUtils.loadStyleSheet(module, 'styles/styles.css');
+
+
     //CONST
     const COMMAND_ID = "cacique.caciquetemplate",
-          COMMAND_NAME = "Cacique Templates";
+        COMMAND_NAME = "Cacique Templates";
 
     function init() {
-        
+
         currentDoc = DocumentManager.getCurrentDocument();
         editor = EditorManager.getCurrentFullEditor();
-        
-        
+
+
         var cursorPos = editor.getCursorPos();
 
         var text = currentDoc.getLine(cursorPos.line).trim();
 
-        if(text==null || text==""){
+        if (text == null || text == "") {
             showCaciqueDialog();
         } else {
             var ar = text.split("_");
-            if(ar.length == 1){
-                if(ar[0]=="html") insertByName("basic_html", true);
+            if (ar.length == 1) {
+                if (ar[0] == "html") insertByName("basic_html", true);
                 else showCaciqueDialog();
-            }else if(ar.length == 2){
-                if(ar[0]=="js" && ar[1]=="fori") insertText("for(var i=0; i<10; i++){\n\n}", text.length);
-                else if(ar[0]=="html" && ar[1]=="css") insertByName("link_css", true);
-                else if(ar[0]=="html" && ar[1]=="canvas") insertByName("basic_canvas", true);
+            } else if (ar.length == 2) {
+                if (ar[0] == "js" && ar[1] == "fori") insertText("for(var i=0; i<10; i++){\n\n}", text.length);
+                else if (ar[0] == "html" && ar[1] == "css") insertByName("link_css", true);
+                else if (ar[0] == "html" && ar[1] == "canvas") insertByName("basic_canvas", true);
                 else showCaciqueDialog();
-            } else if(ar.length == 3){
-                if(ar[0]=="js" && ar[1]=="fori") insertText("for(var i=0; i<"+ar[2]+"; i++){\n\n}", text.length);
+            } else if (ar.length == 3) {
+                if (ar[0] == "js" && ar[1] == "fori") insertText("for(var i=0; i<" + ar[2] + "; i++){\n\n}", text.length);
                 else showCaciqueDialog();
             } else {
                 showCaciqueDialog();
             }
-        } 
+        }
     }
-    
-    function showCaciqueDialog(){
+
+    function showCaciqueDialog() {
         showDialog(modal);
-        document.getElementById('html_select').onchange = function(){replace('html_select')};
-        document.getElementById('js_select').onchange = function(){
-            switch(document.getElementById('js_select').value){
+        document.getElementById('html_select').onchange = function () {
+            replace('html_select')
+        };
+        document.getElementById('js_select').onchange = function () {
+            switch (document.getElementById('js_select').value) {
                 case "fori":
                     insertText("for(var i=0; i<10; i++){\n\n}");
                     break;
@@ -68,31 +84,34 @@ define(function (require, exports, module) {
             myDialog.close();
         };
     }
-    
-    function insertByName(name, offset){
-        var off = (offset==null)?false:true;
-        for(var i=0; i<elements.length; i++){
-            if(elements[i].name == name) {
-                if(offset==false) insertText(elements[i].element);
+
+    function insertByName(name, offset) {
+        var off = (offset == null) ? false : true;
+        for (var i = 0; i < elements.length; i++) {
+            if (elements[i].name == name) {
+                if (offset == false) insertText(elements[i].element);
                 else insertText(elements[i].element, name.length);
-                break;   
+                break;
             }
-        } 
+        }
     }
-    
-    function replace(selectName){
+
+    function replace(selectName) {
         var option = document.getElementById(selectName).value;
         insertByName(option);
         myDialog.close();
     }
-    
+
     //INSERT TEXT ON PAGE
     function insertText(text, offset) {
-        var off = (offset==null)?0:offset;
+        var off = (offset == null) ? 0 : offset;
         currentDoc = DocumentManager.getCurrentDocument();
         editor = EditorManager.getCurrentFullEditor();
         var pos = editor.getCursorPos();
-        var pos2 = {line: pos.line, ch: pos.ch - off};
+        var pos2 = {
+            line: pos.line,
+            ch: pos.ch - off
+        };
         currentDoc.replaceRange(text, pos, pos2);
     }
 
@@ -105,13 +124,23 @@ define(function (require, exports, module) {
     function showDialog(dialog) {
         myDialog = Dialogs.showModalDialog(DefaultDialogs.DIALOG_ID_INFO, COMMAND_NAME, dialog, [
             {
-                className: Dialogs.DIALOG_BTN_CLASS_NORMAL, 
+                className: Dialogs.DIALOG_BTN_CLASS_NORMAL,
                 id: "cancel",
                 text: "Cancel"
                 }
             ]);
     }
-    
+
+    // Add Toolbar Button
+    $(document.createElement('a'))
+        .attr('id', 'cacique-icon')
+        .attr('href', '#')
+        .attr('title', 'Cacique')
+        .on('click', function () {
+            showCaciqueDialog();
+        })
+        .appendTo($('#main-toolbar .buttons'));
+
     //ON LOAD APP
     AppInit.appReady(function () {
         CommandManager.register(COMMAND_NAME, COMMAND_ID, init);
