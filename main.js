2da14177516544ecb1f6c6ba2b2e0f3fde1d4758
Pedro Cacique <phcacique@gmail.com>
2021-02-01 14:28:29 -0300

    Add files via upload
    Bug fix

--- main.js
+++ main.js
@@ -26,6 +26,10 @@ define(function (require, exports, module) {
             name: "link_css",
             element: require('text!html/link_css.html')
         },
+        {
+            name: "link_js",
+            element: require('text!html/link_js.html')
+        },
                    ];
 
     var currentDoc = DocumentManager.getCurrentDocument();
@@ -33,7 +37,7 @@ define(function (require, exports, module) {
     var myDialog;
 
     ExtensionUtils.loadStyleSheet(module, 'styles/styles.css');
-
+    var snippets = [];
 
     //CONST
     const COMMAND_ID = "cacique.caciquetemplate",
@@ -41,9 +45,10 @@ define(function (require, exports, module) {
 
     function init() {
 
+        loadFileSameDir("cacique.txt", getSnippets);
+
         currentDoc = DocumentManager.getCurrentDocument();
         editor = EditorManager.getCurrentFullEditor();
-
 
         var cursorPos = editor.getCursorPos();
 
@@ -51,16 +56,55 @@ define(function (require, exports, module) {
 
         if (text == null || text == "") {
             showCaciqueDialog();
+        } if (text == "html"){
+            insertByName("basic_html", true);
         } else {
             var ar = text.split("_");
+            
+            var temp = ar[0].split(" ");
+            var head = "";
+            if (ar.length == 2 && temp.length>0) {
+                for(var i=0; i<temp.length-1; i++){
+                    head += temp[i]+" ";
+                }
+                ar[0] = temp[temp.length - 1];
+            }
+            
+            temp = ar[1].split(" ");
+            var tail = "";
+            if (ar.length == 2 && temp.length>0) {
+                ar[1] = temp[0];
+                for(var i=1; i<temp.length; i++){
+                    tail += temp[i]+" ";
+                }
+            }
+            if (!head.replace(/\s/g, '').length) { head = ""};
+            if (!tail.replace(/\s/g, '').length) { tail = ""};
+            
+            alert(ar);
+            
             if (ar.length == 1) {
-                if (ar[0] == "html") insertByName("basic_html", true);
-                else showCaciqueDialog();
+                if (ar[0] == "html") {insertByName("basic_html", true);}
+                else {showCaciqueDialog();}
             } else if (ar.length == 2) {
                 if (ar[0] == "js" && ar[1] == "fori") insertText("for(var i=0; i<10; i++){\n\n}", text.length);
-                else if (ar[0] == "html" && ar[1] == "css") insertByName("link_css", true);
-                else if (ar[0] == "html" && ar[1] == "canvas") insertByName("basic_canvas", true);
-                else showCaciqueDialog();
+                else if (ar[0] == "html" && ar[1] == "css") {insertByName("link_css", true)}
+                else if (ar[0] == "html" && ar[1] == "js") {insertByName("link_js", true)}
+                else if (ar[0] == "html" && ar[1] == "canvas") {insertByName("basic_canvas", true);}
+                else {
+                    var found = false;
+                    var txt = "";
+                    if (ar[0] == "c") {
+                        for (var i = 0; i < snippets.length; i++) {
+                            if (snippets[i].name == ar[1]) {
+                                txt = head + snippets[i].content + tail;
+                                found = true;
+                            }
+                        }
+                    }
+                    if (!found) showCaciqueDialog();
+                    else insertText(txt, text.length);
+                }
             } else if (ar.length == 3) {
                 if (ar[0] == "js" && ar[1] == "fori") insertText("for(var i=0; i<" + ar[2] + "; i++){\n\n}", text.length);
                 else showCaciqueDialog();
@@ -70,10 +114,15 @@ define(function (require, exports, module) {
         }
     }
 
+
+
     function showCaciqueDialog() {
         showDialog(modal);
         document.getElementById('html_select').onchange = function () {
             replace('html_select')
+        };
+        document.getElementById('teste').onclick = function () {
+            teste();
         };
         document.getElementById('js_select').onchange = function () {
             switch (document.getElementById('js_select').value) {
@@ -103,8 +152,8 @@ define(function (require, exports, module) {
     }
 
     //INSERT TEXT ON PAGE
-    function insertText(text, offset) {
-        var off = (offset == null) ? 0 : offset;
+    function insertText(text, offset = 0) {
+        var off = offset;
         currentDoc = DocumentManager.getCurrentDocument();
         editor = EditorManager.getCurrentFullEditor();
         var pos = editor.getCursorPos();
@@ -129,6 +178,46 @@ define(function (require, exports, module) {
                 text: "Cancel"
                 }
             ]);
+    }
+
+    function teste() {
+        loadFileSameDir("cacique.txt", getSnippets);
+        myDialog.close();
+        Dialogs.showModalDialog(DefaultDialogs.DIALOG_ID_INFO, "Success", "Snippets loaded!");
+        
+    }
+
+    function getSnippets(text) {
+        snippets = [];
+        var lines = text.split('\n');
+        if (lines.length > 0) {
+            for (var i = 0; i < lines.length; i++) {
+                var s = lines[i].split('::');
+                if (s.length == 2) {
+                    var snippet = {
+                        name: s[0],
+                        content: s[1]
+                    };
+                    snippets.push(snippet);
+                } else {
+                    if (snippets.length > 0) {
+                        snippets[snippets.length - 1].content += "\n"+lines[i];
+                    }
+                }
+            }
+        }
+    }
+
+    function loadFileSameDir(fileName, sFunction = function (t) {}, eFunction = function (e) {}) {
+        var d = DocumentManager.getCurrentDocument();
+        var file = FileSystem.getFileForPath(d.file.parentPath + fileName);
+        var promise = FileUtils.readAsText(file); // completes asynchronously
+        promise.done(function (text) {
+                sFunction(text);
+            })
+            .fail(function (errorCode) {
+                eFunction(errorCode);
+            });
     }
 
     // Add Toolbar Button
